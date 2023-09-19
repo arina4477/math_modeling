d8150a80bc948a9ad3e73928d67799b7eb58981d
arina4477 <a.golubeva2007@mail.ru>
2023-09-19 14:40:13 +0200

    ouyug

---
+++ lec_1_datatypes.py
@@ -0,0 +1,19 @@
+type 
+
+type(3)
+
+print(type(3))
+
+print(type(3.4))
+
+print(type('Молодец!'))
+
+a = 'molodec'
+print(type(a))
+
+a = ['molodec', 1, 'круто', 6, 8]
+print(type(a))
+
+print(type(True))
+
+print(type(None))
