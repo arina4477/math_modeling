7f49263b835bb7c11d793822129b23bef7bb977a
arina4477 <144795495+arina4477@users.noreply.github.com>
2023-10-02 22:00:09 +0200

    Add files via upload

---
+++ lab_1_task6.py
@@ -0,0 +1,12 @@
+a = []
+b = input('возраст: ')
+c = input('пол: ')
+d = input('имя: ')
+e = input('город : ')
+f = input('класс: ')
+
+a.append(b)
+a.append(c)
+a.append(d)
+a.append(e)
+a.append(f)
