415b9b2e926f65183dd84aa022144f39f267dea9
arina4477 <144795495+arina4477@users.noreply.github.com>
2023-11-27 21:56:13 +0200

    uhhi

--- lab_3_task4.py
+++ lab_3_task4.py
@@ -3,10 +3,13 @@ N = 5
 M = 4
 
 A = np.zeros((N, M))
-print(A)
 
-for i in range(0 , M):
+
+for i in range(0 , N):
     for j in range (0 ,M):
         A[i, j] = np.sin(N * i - M * j)
+        if A[i, j] < 0:
+            A[i, j] = 0
+            
 
 print(A)
---
+++ lab_3_task5.py
@@ -0,0 +1,11 @@
+import numpy as np
+from lab_3_task4 import A as a
+
+print(4)
+
+a[::1, 1] = a[::1, 0]:
+
+
+a[::1, 0] = a[::1, 1]
+
+print(a)
