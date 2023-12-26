11acf3c0c031c4c08414ffe38f93677e16eeec2b
arina4477 <144795495+arina4477@users.noreply.github.com>
2023-12-26 13:39:31 +0200

    7y

---
+++ lec_5_sysos.py
@@ -0,0 +1,12 @@
+import sys, os
+
+print(os.getcwd())
+
+os.system('echo hi!')
+#os.system('C:/Users/inf3-2/AppData/Local/Programs/Python/Python311/python.exe c:/Users/inf3-2/Documents/GitHub/math_modeling/lec_5_sysos.py')
+
+print("python version is:", sys.version)
+print(sys.path)
+print(sys.platform)
+
+print(dir(sys))
