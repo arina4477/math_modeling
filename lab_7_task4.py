3753c91349b4277c6034f3b8db3a6e6211348f73
arina4477 <144795495+arina4477@users.noreply.github.com>
2024-03-18 20:40:26 +0200

    pouihjhp

---
+++ lab_7_task4.py
@@ -0,0 +1,31 @@
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+def circle_move (C, x0, y0, D):
+
+    for i in range(0, 8):
+        n = i-1
+        xi = (xn)**2-(yn)**2+C
+        yi = 2*x(i-1)*y(i-1)+D
+    return xi, yi
+
+def animate(i):
+    ball.set_data(circle_move(D=0.33, x0 = 0.01, y0=0.01, C = 0.3))
+
+if __name__ == '__main__':
+    fig, ax = plt.subplots()
+    ball, = plt.plot([], [], '-', color='r', label ='Ball')
+
+    edge = 3
+    plt.axis('equal')
+    ax.set_xlim(-edge, edge)
+    ax.set_ylim(-edge, edge)
+
+    ani = FuncAnimation(fig,
+                        animate,
+                        frames=100,
+                        interval=30
+                        )
+    
+    ani.save('animation_7.gif')
