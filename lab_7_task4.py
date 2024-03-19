61367a6b5631859fc81b739906c4af5941fd1064
arina4477 <144795495+arina4477@users.noreply.github.com>
2024-03-19 14:04:27 +0200

    hsty

--- lab_7_task4.py
+++ lab_7_task4.py
@@ -2,30 +2,38 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt 
 import numpy as np 
 
-def circle_move (C, x0, y0, D):
+N = 10
 
-    for i in range(0, 8):
-        n = i-1
-        xi = (xn)**2-(yn)**2+C
-        yi = 2*x(i-1)*y(i-1)+D
-    return xi, yi
+D=0.33
+x0 = 0.01
+y0=0.01
+C = 0.3
+
+x = []
+x.append(x0)
+y = []
+y.append(y0)
+for i in range(1, N):
+    x.append(x[i-1]**2-y[i-1]**2+C)
+    y.append(2*x[i-1]*y[i-1]+D)
+
 
 def animate(i):
-    ball.set_data(circle_move(D=0.33, x0 = 0.01, y0=0.01, C = 0.3))
+    ball.set_data(x[:i], y[:i])
 
 if __name__ == '__main__':
     fig, ax = plt.subplots()
-    ball, = plt.plot([], [], '-', color='r', label ='Ball')
+    ball, = plt.plot([], [], 'o', color='r', label ='Ball')
 
-    edge = 3
+    edge = 3  
     plt.axis('equal')
     ax.set_xlim(-edge, edge)
     ax.set_ylim(-edge, edge)
 
     ani = FuncAnimation(fig,
                         animate,
-                        frames=100,
-                        interval=30
+                        interval=100,
+                        frames = N
                         )
     
     ani.save('animation_7.gif')
