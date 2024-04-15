bcf35b812c8811498af689d1228d9353bc0f28db
arina4477 <144795495+arina4477@users.noreply.github.com>
2024-04-15 21:13:56 +0200

    etrgth

--- lec_1_approximation.py
+++ lec_1_approximation.py
@@ -20,5 +20,74 @@ x = 4 + 3 * np.cos(t)
 y = 7 + 5 * np.sin(t)
 ax.plot(x, y, '-', lw=2, color = 'k')
 
+t = np.linspace(np.pi/2-np.pi/3.5,np.pi/2+np.pi/3.5, 100 )
+x = 3 + 1*np.cos(t)
+y = 4.75 + 1*np.sin(t)
+ax.plot(x, y, '-', lw=2, color='k')
+
+t = np.linspace(-np.pi/2+np.pi/3.5,-np.pi/2-np.pi/3.5, 100 )
+x = 3 + 1*np.cos(t)
+y = 6 + 1*np.sin(t)
+ax.plot(x, y, '-', lw=2, color='k')
+
+t = np.linspace(np.pi/2-np.pi/3.5,np.pi/2+np.pi/3.5, 100 )
+x = 5 + 1*np.cos(t)
+y = 4.75 + 1*np.sin(t)
+ax.plot(x, y, '-', lw=2, color='k')
+
+t = np.linspace(-np.pi/2+np.pi/3.5,-np.pi/2-np.pi/3.5, 100 )
+x = 5 + 1*np.cos(t)
+y = 6 + 1*np.sin(t)
+ax.plot(x, y, '-', lw=2, color='k')
+
+t = np.linspace(-np.pi/2,3*np.pi/2, 100 )
+x = 5 + 0.2*np.cos(t)
+y = 5.32 + 0.2*np.sin(t)
+ax.plot(x, y, '-', lw=2, color='k')
+
+t = np.linspace(-np.pi/2,3*np.pi/2, 100 )
+x = 3 + 0.2*np.cos(t)
+y = 5.32 + 0.2*np.sin(t)
+ax.plot(x, y, '-', lw=2, color='k')
+
+t = np.linspace(np.pi, 3*np.pi/2, 100 )
+x = 4.5 + 0.5*np.cos(t)
+y = 3.8 + 0.6*np.sin(t)
+ax.plot(x, y, '-', lw=2, color='k')
+
+t = np.linspace(3/2*np.pi, 2*np.pi, 100 )
+x = 3.5 + 0.5*np.cos(t)
+y = 3.8 + 0.6*np.sin(t)
+ax.plot(x, y, '-', lw=2, color='k')
+
+t = np.linspace(-np.pi/2,3*np.pi/2, 100 )
+x = 4 + 0.2*np.cos(t)
+y = 4 + 0.1*np.sin(t)
+ax.plot(x, y, '-', lw=2, color='k')
+
+x = [5, 6]
+y = [4, 4.8]
+ax.plot(x, y, '-', lw=2, color = 'k')
+
+x = [5, 6]
+y = [4, 4.1]
+ax.plot(x, y, '-', lw=2, color = 'k')
+
+x = [5, 5.9]
+y = [4, 3.5]
+ax.plot(x, y, '-', lw=2, color = 'k')
+
+x = [3, 2]
+y = [4, 4.8]
+ax.plot(x, y, '-', lw=2, color = 'k')
+
+x = [3, 2]
+y = [4, 4.1]
+ax.plot(x, y, '-', lw=2, color = 'k')
+
+x = [3, 2.1]
+y = [4, 3.5]
+ax.plot(x, y, '-', lw=2, color = 'k')
+
 plt.axis('equal')
 plt.savefig('cat.png')
--- lec_1_imageapproximation.py
+++ lec_1_imageapproximation.py
@@ -27,4 +27,29 @@ x = 380 + 60 * np.cos(t)
 y = 320 - 50 * np.sin(t)
 ax.plot(x, y, '-', lw=2, color = 'k')
 
+t =  np.linspace(3 * np.pi/2, 2 * np.pi, 200)
+x = 380 + 100 * np.cos(t)
+y = 160 - 110 * np.sin(t)
+ax.plot(x, y, '-', lw=2, color = 'k')
+
+t =  np.linspace(np.pi, 3 * np.pi/2, 200)
+x = 610 + 130 * np.cos(t)
+y = 155 - 45 * np.sin(t)
+ax.plot(x, y, '-', lw=2, color = 'k')
+
+t =  np.linspace(2 * np.pi, 5 * np.pi/2, 200)
+x = 610 + 105 * np.cos(t)
+y = 300 - 100 * np.sin(t)
+ax.plot(x, y, '-', lw=2, color = 'k')
+
+x = [715, 690]
+y = [300, 580]
+ax.plot(x, y, '-', lw=2, color = 'k')
+
+t =  np.linspace(np.pi/2, np.pi,  200)
+x = 690 + 60 * np.cos(t)
+y = 705 - 125 * np.sin(t)
+ax.plot(x, y, '-', lw=2, color = 'k')
+
+
 plt.savefig('horse.png')
