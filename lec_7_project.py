00a3cd94ccd47979e17fce5b5deb47532d0efc7c
arina4477 <144795495+arina4477@users.noreply.github.com>
2024-04-16 14:20:09 +0200

    ertgest

--- lec_7_project.py
+++ lec_7_project.py
@@ -2,30 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
 
-fig, ax = plt.subplots()
-
-
-
-anim_object, = plt.plot([], [], '-', lw=2)
-
 xdata, ydata = [], []
-
-ax.set_xlim(0, 20)
-ax.set_ylim(-10, 10)
-
-def update(frame, a=2):
-    xdata.append(a * (frame - np.sin(frame)))
-    ydata.append(a * (1 - np.cos(frame)))
-    anim_object.set_data(xdata, ydata)
-    return anim_object,
-
-ani = FuncAnimation(fig,
-                    update,
-                    frames=np.arange(0, 3*np.pi, 0.1),
-                    interval=1
-                    )
-
-ani.save('animation_7.gif')
 
 def circle_move(R, vx0, vy0, time):
     x0 = vx0 * time
@@ -35,12 +12,19 @@ def circle_move(R, vx0, vy0, time):
     y = y0 + R*np.sin(alpha)
     return x, y
 
-def animate(i):
-    ball.set_data(circle_move(R=2, vx0 = 0.09, vy0=0.0001, time = i))
+
+def update(frame):
+    xdata.append(2 * (frame - np.sin(frame)))
+    ydata.append(2 * (1 - np.cos(frame)))
+    anim_object.set_data(xdata, ydata)
+
+    ball.set_data(circle_move(R=2, vx0 = 0.09, vy0=0.0001, time = frame))
+
 
 if __name__ == '__main__':
     fig, ax = plt.subplots()
     ball, = plt.plot([], [], '-', color='r', label ='triangle')
+    anim_object, = plt.plot([], [], '-', lw=2)
 
     edge = 8
     plt.axis('equal')
@@ -48,9 +32,10 @@ if __name__ == '__main__':
     ax.set_ylim(-edge, edge)
 
     ani = FuncAnimation(fig,
-                        animate,
-                        frames=100,
-                        interval=30
-                        )
+                    update,
+                    frames=np.arange(0, 3*np.pi, 0.1),
+                    interval=100
+                    )
+
     
     ani.save('animation_8.gif')
