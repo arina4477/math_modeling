85bedc5b659eef2830d3de1d5afe46470b422d8b
arina4477 <144795495+arina4477@users.noreply.github.com>
2024-05-07 14:15:32 +0200

    lijh9yugh9y

---
+++ lec_1_scalarfields.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random 
+
+fig, ax = plt.subplots()
+
+N = 1000
+x, y = np.zeros(N), np.zeros(N)
+
+for i in range(N):
+    x[i] = random.uniform(0, 1)
+    y[i] = random.uniform(0, 1)
+
+scalar_field = 50 * np.sqrt(x**2+y**2)
+
+sc_plot = ax.scatter(x, y, c = scalar_field)
+ax.set_xlabel('координата x, м')
+ax.set_ylabel('координата y, м')
+
+cbar = fig.colorbar(sc_plot)
+
+cbar.set_label('Скалярное поле температуры, C')
+plt.savefig('dich.png')
+
+
+
+
+
+
+
+
+
+
+
+
